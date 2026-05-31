ab2dccb702cd59678213285e0c8f43785a5d4422
Achyuthvenkat <achyuthvenkat07@gmail.com>
2026-05-31 11:20:42 +0530

    fixed some bugs

--- frontend/lib/core/network/dio_client.dart
+++ frontend/lib/core/network/dio_client.dart
@@ -3,7 +3,7 @@ import 'package:dio/dio.dart';
 class DioClient {
   static final Dio _dio = Dio(
     BaseOptions(
-      baseUrl: 'http://127.0.0.1:8081/api', // Use localhost for web, 192.168.1.9 for physical device
+      baseUrl: 'http://192.168.1.5:8081/api', // Use local IP address for physical devices and emulators
       connectTimeout: const Duration(seconds: 10),
       receiveTimeout: const Duration(seconds: 10),
       headers: {
